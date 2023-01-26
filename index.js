65e0186dbdfaf72fd414e430800b2d76fe1c05e2
Alessandra Scarpellini <scaalessandra@icloud.com>
2023-01-26 20:28:10 +0100

    chat working in local

--- index.js
+++ index.js
@@ -1,33 +1,56 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 4000;
 
-const http = require('http').Server(app);
-const cors = require('cors');
+const http = require("http").Server(app);
+const cors = require("cors");
 
 app.use(cors());
 
-const socketIO = require('socket.io')(http, {
-    cors: {
-        origin: "http://localhost:3000"
-    }
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
 });
+
+// Active users in the chat
+let users = [];
 
 //Add this before the app.get() block
-socketIO.on('connection', (socket) => {
-    console.log(`⚡: ${socket.id} user just connected!`);
-    socket.on('disconnect', () => {
-      console.log('🔥: A user disconnected');
-    });
-});
+socketIO.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  //sends the message to all the users on the server
+  socket.on("message", (data) => {
+    socketIO.emit("messageResponse", data);
+  });
+  //recive and send data of who is typing
+  socket.on("typing", (data) => socket.broadcast.emit("typingResponse", data));
 
-app.get('/api', (req, res) => {
-  res.json({
-    message: 'Chatty chat',
+  //Listens when a new user joins the server
+  socket.on("newUser", (data) => {
+    //Adds the new user to the list of users
+    users.push(data);
+    // console.log(users);
+    //Sends the list of users to the client
+    socketIO.emit("newUserResponse", users);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+    //Updates the list of users when a user disconnects from the server
+    users = users.filter((user) => user.socketID !== socket.id);
+    // console.log(users);
+    //Sends the list of users to the client
+    socketIO.emit("newUserResponse", users);
+    socket.disconnect();
   });
 });
 
-
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Chatty chat",
+  });
+});
 
 http.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
